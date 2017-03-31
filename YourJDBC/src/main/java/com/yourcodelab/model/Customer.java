a354df1756149236352d38454e5c825598415ed8
03119961914 <03119961914@full-cosin-01.sefaad.local>
2017-03-31 12:04:57 -0300

    Creating the Customer class.

---
+++ YourJDBC/src/main/java/com/yourcodelab/model/Customer.java
@@ -0,0 +1,26 @@
+package com.yourcodelab.model;
+
+public class Customer {
+	private Integer customerID;
+	private String name;
+	private String email;
+	
+	public Integer getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(Integer customerID) {
+		this.customerID = customerID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
